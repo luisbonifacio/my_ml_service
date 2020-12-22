26f82af05d13077d35b86eed511b7581f398885e
luisbonifacio <luis.g.bonifacio@gmail.com>
2020-12-22 00:04:10 -0300

    endpoints models

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', # add django rest framework
+    'apps.endpoints' # add apps
 ]
 
 MIDDLEWARE = [
